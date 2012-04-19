2eea6df2fde48b097cea4dd2b767d985aaddadd0
Kevin Rayfield <projektidiot@gmail.com>
2012-04-19 18:03:01 -0400

    slowly adding.

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 	$('input[type=submit]').click(function(event) {
 		event.preventDefault();
+		$("#results").html("");
+		
+		var input = $('input[type=text]').val();
+		
+		$.ajax({
+		});
 	});
 });
