83bb9e89fc06282a028e2c49340a09b472baeab8
Amanda <abeisler@knights.ucf.edu>
2012-04-24 02:27:52 -0400

    added GetLyrics function

--- getLyric.php
+++ getLyric.php
@@ -1,20 +1,18 @@
-<?php 
+<?php  
 
-$id = $_POST['id'];
+$id = $_POST['id']; 
 
-$url = "http://api.chartlyrics.com/apiv1.asmx/GetLyric?";
-$url .= $id;
+$url = "http://api.chartlyrics.com/apiv1.asmx/GetLyric?".$id; 
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-$curlResponse = curl_exec($ch);
-curl_close($ch);   
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_HEADER, 0); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_URL, $url); 
+$curlResponse = curl_exec($ch); 
+curl_close($ch);    
 
-$xmlObject = simplexml_load_string($curlResponse); 
+$xmlObject = simplexml_load_string($curlResponse);  
 
-
-	echo "<p>".$xmlObject->GetLyricResult->Lyric."</p>";
-	echo "<a href='".$url."'>".$url."</a>";
+echo "<li><h2>Lyrics: ".$xmlObject->Lyric."</h2></li>";
+	
 ?>
--- searchLyricText.php
+++ searchLyricText.php
@@ -1,19 +1,19 @@
-<?php 
+<?php  
 
-$lyrics = str_replace(' ', '+', $_POST['term']);
+$lyrics = str_replace(' ', '+', $_POST['term']); 
 
-$url  = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricText?lyricText=".$lyrics;
+$url  = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricText?lyricText=".$lyrics; 
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-$curlResponse = curl_exec($ch);
-curl_close($ch);
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_HEADER, 0); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_URL, $url); 
+$curlResponse = curl_exec($ch); 
+curl_close($ch); 
 
-$xmlObject = simplexml_load_string($curlResponse); 
+$xmlObject = simplexml_load_string($curlResponse);  
 
-foreach($xmlObject->SearchLyricResult as $searchLyrics) {
-	echo "<li><h1 alt='lyricID=".$searchLyrics->LyricId."&lyricCheckSum=".$searchLyrics->LyricChecksum."'>".$searchLyrics->Artist." - ".$searchLyrics->Song."</h1></li>";
-}
+foreach($xmlObject->SearchLyricResult as $searchLyrics) { 
+    echo "<li><h1 alt='lyricID=".$searchLyrics->LyricId."&lyricCheckSum=".$searchLyrics->LyricChecksum."'>".$searchLyrics->Artist." - ".$searchLyrics->Song."</h1></li>"; 
+	} 
 ?>
