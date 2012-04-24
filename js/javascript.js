cdbd441372e9c198a76c25c3baa18eab0e746874
Amanda <abeisler@knights.ucf.edu>
2012-04-24 02:26:28 -0400

    just poking around

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,61 @@
+// JavaScript Document
+
 $(document).ready(function() {
+
 	$('input[type=submit]').click(function(event) {
+
 		event.preventDefault();
+
 		$("#selected").html("");
+
 		
+
 		var input = $('input[type=text]').val();
+
 		
+
 		$.ajax({
+
 			url: 'searchLyricText.php',
+
 			type: 'POST',
+
 			data: {'term':input},
+
 			success: function(result) {
+
 				$("#lyrics ul").html(result);
+
 				
+
 				$("h1").click(function(){
+
 										
+
 					var id = $(this).attr('alt');
+
 					$.ajax({
+
 						url: 'getLyric.php',
+
 						type: 'POST',
+
 						data: {'id':id},
+
 						success: function(lyrics){
+
 							$("#selected").html(lyrics);
+
 						},
+
 					});
+
 				});
+
 			}
+
 		});
+
 	});
+
 });
